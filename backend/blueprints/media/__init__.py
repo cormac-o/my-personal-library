293bfd602bdcc91cb2552ddf70a6a26fabba64fe
cormac-o <cormaco118@gmail.com>
2026-09-06 23:33:02 +0100

    Spelling error

--- backend/blueprints/media/__init__.py
+++ backend/blueprints/media/__init__.py
@@ -2,9 +2,9 @@
 # This is the file registered for media items with endpoints imported.
 # Endpoints will be implemented in their respective files (books.py, movies.py, etc.)
 
-from flask import blueprint
+from flask import Blueprint
 
-media_bp = blueprint('media', __name__)
+media_bp = Blueprint('media', __name__)
 
 from .books import *
 from .movies import *
